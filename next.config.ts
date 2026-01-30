f658214d77a3a5c49c05c3df29726fa8ffebbe19
Borbobb <madeadisujaya23@gmail.com>
2026-01-31 00:42:44 +0800

    Update deployment configuration and Next.js settings for improved build process and static hosting compatibility; add npm install and build steps, configure output and image handling options.

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,16 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  // Ini wajib supaya Next.js membuat folder "out/"
+  output: "export",
+
+  // Ini wajib supaya next/image tidak error di static hosting
+  images: {
+    unoptimized: true,
+  },
+
+  // Optional: supaya URL halaman jadi rapi
+  trailingSlash: true,
 };
 
 export default nextConfig;
